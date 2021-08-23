30e15b5f8d0e0faf4d844b29f929f492f999b7a3
Ilya <jawa009@gmail.com>
2021-08-23 11:20:23 +0300

    init

--- app/src/main/java/com/example/loginfirebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/loginfirebaseapp/MainActivity.kt
@@ -9,12 +9,5 @@ import com.google.firebase.auth.FirebaseAuth
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    fun Logout(view: View) {
-        FirebaseAuth.getInstance().signOut()
-        startActivity(Intent(applicationContext, LoginActivity::class.java))
-        finish()
     }
 }
